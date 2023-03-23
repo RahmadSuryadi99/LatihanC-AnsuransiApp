be55e84954c1f76ff057220a4f539b74f239c000
YadiLaptopIglo <Rahmadtkd13@gmail.com>
2023-03-23 13:20:45 +0700

    Create ansuransiApp

---
+++ LatihanOOPAns/LatihanOOPAns/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LatihanOOPAns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
